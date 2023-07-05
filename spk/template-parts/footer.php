b26cd19dc58f18488fbe2f74f6bb23a67f742df6
Agung Gumilang <78994125+wh4td0y0um34n@users.noreply.github.com>
2023-07-05 03:48:38 -0700

    Add files via upload

---
+++ spk/template-parts/footer.php
@@ -0,0 +1,18 @@
+	
+	</div><!-- #main -->
+	
+	<footer id="footer">
+		<div class="container">
+			<p>Dibuat oleh <a href="https://github.com/wh4td0y0um34n?tab=repositories" target="_blank">Agung Gumilang</a></p>
+		</div>
+	</footer>
+
+	</div><!-- #page -->
+</body>
+</html>
+<?php
+if(isset($pdo)) {
+	// Tutup Koneksi
+	$pdo = null;
+}
+?>
---
+++ spk/template-parts/sidebar-kambing.php
@@ -0,0 +1,8 @@
+<aside class="sidebar">
+	<div class="widget">
+		<ul>
+			<li><a href="list-kambing.php"><span class="fa fa-bars"></span> List Supplier</a></li>
+			<li><a href="tambah-kambing.php"><span class="fa fa-plus"></span> Tambah Supplier</a></li>
+		</ul>
+	</div>
+</aside>
